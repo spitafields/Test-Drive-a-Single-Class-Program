8d0fa6d52a24819153879a111fa5c12dbae60eab
alessandro spitaleri <alessandrospitaleri5@gmail.com>
2023-04-23 19:40:49 +0200

    exercise two

---
+++ spec/diary_entry_spec.rb
@@ -0,0 +1,85 @@
+Require "diary_entry"
+
+Rspec.describe DiaryEntry do
+  it "constructs" do
+    diary_entry = DiaryEntry.new ("my_title", "my_contents")
+    expect(diary_entry.title).to eq "my_title"
+    expect(diary_entry.contents).to eq "my_contents"
+  end
+
+  describe "#count_words" do
+    it "returns the number of words in the contents" do
+      diary_entry = DiaryEntry.new ("my_title", "some contents here")
+      expect(diary_entry.count_words).to eq 3
+    end
+
+    it "returns zero when contents  is empty"
+      diary_entry = DiaryEntry.new ("my_title", "")
+      expect(diary_entry.count_words).to eq 0
+    end
+  end
+
+  describe "#reading_time" do
+    context "given a wpn of some sensible number(200)" do
+      it "returns the ceiling of the number of minutes it takes to read the contents" do
+        diary_entry = Diary_Entry.new("my_title", "one " * 550)
+        expect(diary_entry.reading_time(200)).to eq 3
+      end
+    end
+
+    context "given a wpn of 0" do 
+      it "fails" do
+        diary_entry = Diary_Entry.new ("my_title", "one two three")
+        expect { diary_entry.reading_time(0) }.to raise_error "Reading speed must be above zero."
+      end
+    end  
+  end
+
+  describe "#reaing_chunk" do
+    context  "with a contents readable within the given minutes" do
+      it "returns the full contents " do
+        diary_entry = Diary_Entry.new("my_title", "one two three")
+        chunk = diary_entry.reading_chunk(200, 1)
+        expect(chunk).to eq "one two three"
+      end
+    end
+
+    context "given a wpn of 0" do
+      it "fails" do
+        diary_entry = Diary_Entry.new("my_title", "one two three") 
+        expect {diary_entry.reading_chunk(0, 5) }.to raise_error "Reading speed must be above zero."
+      end
+    end
+    
+    context  "with a contents unreadable within the time " do
+      it "returns a readable chunk" do
+        diary_entry = Diary_Entry.new("my_title", "one two three")
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+
+    it "returns the next chunk, next time we are asked " do
+      diary_entry = Diary_Entry.new("my_title", "one two three")
+      diary_entry.reading_chunk(2, 1)
+      chunk = diary_entry.reading_chunk(2, 1)
+      expect(chunk).to eq "three"
+    end
+
+    it "restarts after reading the whole contents" do
+      diary_entry = Diary_Entry.new("my_title", "one two three")
+      diary_entry.reading_chunk(2, 1)
+      diary_entry.reading_chunk(2, 1)
+      chunk = diary_entry.reading_chunk(2, 1)
+      expect(chunk).to eq "one two"
+    end
+  end
+
+    it "restarts if it finishes exactly on the end" do 
+      diary_entry = Diary_Entry.new("my_title", "one two three")
+      diary_entry.reading_chunk(2, 1)
+      diary_entry.reading_chunk(1, 1)
+      chunk = diary_entry.reading_chunk(2, 1)
+      expect(chunk).to eq "one two"
+    end
+  end
+end
